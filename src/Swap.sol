bc539f06fa23967058de8be6721919aa63d1f8b2
Leo Alt <leo@ethereum.org>
2022-10-21 17:13:27 +0200

    init

---
+++ src/Swap.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.17;
+
+contract SocksSwap {
+    mapping (address => uint) left;
+    mapping (address => uint) right;
+
+    address constant socks = 0xdfcCFA821F0bFD9d90746021094FAf6C0f10AB63;
+
+
+}
