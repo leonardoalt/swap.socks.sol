6ef06b7c226af771700077a8ad449cb36b92e8da
Leo Alt <leo@ethereum.org>
2022-10-23 14:38:48 +0200

    buncha

--- src/Swap.sol
+++ src/Swap.sol
@@ -6,50 +6,69 @@ import {ERC1155TokenReceiver} from "solmate/tokens/ERC1155.sol";
 import "./Socks.sol";
 
 /// @title Swap contract for Solidity socks.
-/// @dev One invariant of this contract is that users can always either swap or withdraw, therefore their deposited tokens never get stuck.
+/// @dev One invariant of this contract is that users can, after a deposit, always either swap or withdraw, therefore their deposited tokens never get stuck.
+/// A few ways to represent that:
+/// - After a successful call to `deposit(from)` from user U, the next call from U to either `swapAfterDeposit(from, to)` or `withdraw()` cannot revert.
+/// - balanceOf[msg.sender][id] > 0 => socks.balanceOf[address(this)][Sock.Left] > 0 || socks.balanceOf[address(this)][Sock.Left]
+/// The property above must be true for the swap market itself, regardless of calls to `inflate`.
 contract SocksSwap is ERC1155TokenReceiver {
     Socks constant socks = Socks(0xdfcCFA821F0bFD9d90746021094FAf6C0f10AB63);
     uint256 constant BOOTSTRAP = 10;
 
-    mapping (address => uint[2]) balanceOf;
+    mapping(address => mapping(Sock => uint256)) balanceOf;
 
     /// @notice Bootstraps the pool.
     constructor() {
-        for (uint i = 0; i < BOOTSTRAP; ++i) {
+        // Let's fill up with whatever at first.
+        inflate(Sock(block.difficulty % 2));
+    }
+
+    /// @notice Mint more socks for the pool contract. Will fail from 9.11.2022.
+    function inflate(Sock expected) public {
+        // Make sure we fill up with a specific needed side.
+        require(block.difficulty % 2 == uint256(expected));
+
+        for (uint256 i = 0; i < BOOTSTRAP;) {
             socks.mint();
+            unchecked {
+                ++i;
+            }
         }
     }
 
-    /// @notice Atomic deposit & swap.
-    function depositAndSwap(Sock from, Sock to) external {
-        deposit(from);
-        swap(from, to);
+    /// @notice Optimized swap path if the pool has enough tokens.
+    function swap(Sock from, Sock to) external {
+        socks.safeTransferFrom(msg.sender, address(this), uint256(from), 1, "");
+        socks.safeTransferFrom(address(this), msg.sender, uint256(to), 1, "");
     }
 
     /// @notice If there is nothing on the other side of the swap users might need to deposit and swap at different times.
-    function deposit(Sock id) public {
-        socks.safeTransferFrom(msg.sender, address(this), uint(id), 1, "");
-        ++balanceOf[msg.sender][uint(id)];
+    function deposit(Sock id) external {
+        socks.safeTransferFrom(msg.sender, address(this), uint256(id), 1, "");
+        unchecked {
+            ++balanceOf[msg.sender][id];
+        }
     }
 
     /// @notice If there is nothing on the other side of the swap users might need to deposit and swap at different times.
-    function swap(Sock from, Sock to) public {
+    function swapAfterDeposit(Sock from, Sock to) external {
         require(from != to);
-        require(socks.balanceOf(address(this), uint(to)) > 0);
+        require(socks.balanceOf(address(this), uint256(to)) > 0);
 
-        --balanceOf[msg.sender][uint(from)];
-        socks.safeTransferFrom(address(this), msg.sender, uint(to), 1, "");
+        --balanceOf[msg.sender][from];
+        socks.safeTransferFrom(address(this), msg.sender, uint256(to), 1, "");
     }
 
     /// @notice Users can always withdraw their tokens if a swap was not available for a while.
-    function withdraw() public {
-        uint[2] memory balance = balanceOf[msg.sender];
-        balanceOf[msg.sender] = [0, 0];
-        if (balance[0] > 0) {
-            socks.safeTransferFrom(address(this), msg.sender, 0, balance[0], "");
+    function withdraw() external {
+        (uint256 balLeft, uint256 balRight) = (balanceOf[msg.sender][Sock.Left], balanceOf[msg.sender][Sock.Right]);
+        balanceOf[msg.sender][Sock.Left] = 0;
+        balanceOf[msg.sender][Sock.Right] = 0;
+        if (balLeft > 0) {
+            socks.safeTransferFrom(address(this), msg.sender, uint256(Sock.Left), balLeft, "");
         }
-        if (balance[1] > 0) {
-            socks.safeTransferFrom(address(this), msg.sender, 1, balance[1], "");
+        if (balRight > 0) {
+            socks.safeTransferFrom(address(this), msg.sender, uint256(Sock.Right), balRight, "");
         }
     }
 }
